186e57a77b6b4e8f457c413643c97d0568df1b89
yeonkyu <dusrbpoiiij@naver.com>
2019-12-28 00:08:46 +0900

    first in Tongyeong

---
+++ Challenge_1/static/js/script.js
@@ -0,0 +1,15 @@
+// Challenge 1 : Your Age in Days
+
+function ageInDays() {
+    var birthYear = prompt('What year were you born.... Good friend?');
+    var ageInDayes = (2019-birthYear) * 365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are '+ ageInDayes + ' days old.');
+    h1.setAttribute('id','ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset() {
+    document.getElementById('flex-box-result').remove();
+}
